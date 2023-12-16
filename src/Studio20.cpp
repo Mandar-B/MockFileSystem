69c15d227820c68ffa09bbb61ec2df7f79c23a41
Nathaniel Hayman <nathanielhayman@gmail.com>
2023-12-15 18:12:19 -0600

    studio 20 tests

--- src/Studio20.cpp
+++ src/Studio20.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
+#include <string>
 #include "mockos/ImageFile.h"
 #include "mockos/TextFile.h"
 #include "mockos/PasswordProxy.h"
-
-
 #include "mockos/SimpleFileSystem.h"
-#include <iostream>
+#include "mockos/BasicDisplayVisitor.h"
+
+using namespace std;
+
+void print_test(string name, int& num, int res) {
+    cout << num++ << ": " << name << "\t" 
+        << (res ? "ERROR; code " : "SUCCESS") << (res ? to_string(res) : "") << endl;
+}
 
 int main() {
+
+    int test_num = 0;
+
+    cout << "Running Studio 20 Tests...\n" << endl;
+
     TextFile* textFile = new TextFile("ex.txt");
     PasswordProxy* proxy = new PasswordProxy(textFile, "somepassword");
 
     // Write a text file with "Hello" as its contents
-    std::vector<char> data = {'H', 'e', 'l', 'l', 'o'};
-    int writeResult = proxy->write(data);
+    vector<char> data = {'H', 'e', 'l', 'l', 'o'};
+
+    cout << "Provide a correct password to the following prompt:" << endl;
+
+    int test_res = proxy->write(data);
+
+    print_test("Test writing file [correct password]", test_num, test_res);
 
     // Read contents and update to be "Hello World"
-    std::vector<char> contents = proxy->read();
+    vector<char> contents = proxy->read();
     contents.push_back(' ');
     contents.push_back('W');
     contents.push_back('o');
@@ -24,11 +40,34 @@ int main() {
     contents.push_back('l');
     contents.push_back('d');
 
-    // Rewrite file
-    writeResult = proxy->write(contents);
+    cout << "Provide a correct password to the following prompt:" << endl;
 
-    std::cout << "Test rewriting file\t" 
-        << (writeResult ? "ERROR while rewriting file" : "SUCCESS") << std::endl;
+    // Rewrite file
+    test_res = proxy->write(contents);
+
+    print_test("Test rewriting file [correct password]", test_num, test_res);
+
+    test_res = "ex.txt" == proxy->getName();
+
+    print_test("Test getName()", test_num, test_res);
+
+    cout << "Provide a correct password to the following prompt:" << endl;
+
+    AbstractFileVisitor* fv = new BasicDisplayVisitor();
+    proxy->accept(fv);
+
+    print_test("Test accepting basic display visitor [correct password] CONFIRM MANUALLY", test_num, 0);
+
+    cout << "Provide a correct password to the following prompt:" << endl;
+
+    test_res = proxy->write(contents) != 0;
+
+    print_test("Test rewriting file [incorrect password]", test_num, test_res);
+
+    AbstractFileVisitor* fv = new BasicDisplayVisitor();
+    proxy->accept(fv);
+
+    print_test("Test accepting basic display visitor [incorrect password] CONFIRM MANUALLY", test_num, 0);
 
     return 0;
 }
