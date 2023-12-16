a6a71eb9a21cac5a62a3ce9f347c6f36697ea9e5
Mandar <b.mandar@wustl.edu>
2023-12-15 18:17:11 -0600

    more stud 18 updates

--- src/Studio18.cpp
+++ src/Studio18.cpp
@@ -1,89 +1,66 @@
 #include "mockos/SimpleFileSystem.h"
+#include "mockos/SimpleFileFactory.h"
 #include "mockos/TextFile.h"
-#include "mockos/ImageFile.h"
 #include "mockos/Constants.h"
 #include <iostream>
-
+using namespace std;
 int main() {
+    // Initialize a SimpleFileSystem and SimpleFileFactory
     AbstractFileSystem* fileSystem = new SimpleFileSystem();
+    AbstractFileFactory* fileFactory = new SimpleFileFactory();
 
-    // Test Case 1: Create and add a TextFile
-    std::cout << "Test Case 1: Create and add a TextFile";
-    int result = fileSystem->createFile("test.txt");
-    if (result == OK) {
-        std::cout << "File 'test.txt' created successfully.";
+    // Test Case 1: Create and Add TextFiles
+    int createResult1 = fileSystem->createFile("file1.txt");
+    int createResult2 = fileSystem->createFile("file2.txt");
+
+    // Add files to the file system
+    int addResult1 = fileSystem->addFile("file1.txt", fileFactory->createFile("file1.txt"));
+    int addResult2 = fileSystem->addFile("file2.txt", fileFactory->createFile("file2.txt"));
+
+    // Print results
+    cout << "Test Case 1 Results:" << endl;
+    cout << "Create file1.txt result: " << createResult1 << endl;
+    cout << "Create file2.txt result: " << createResult2 << endl;
+    cout << "Add file1.txt result: " << addResult1 << endl;
+    cout << "Add file2.txt result: " << addResult2 << endl;
+
+    // Test Case 2: Open, Write, and Read from TextFiles
+    AbstractFile* file1 = fileSystem->openFile("file1.txt");
+    AbstractFile* file2 = fileSystem->openFile("file2.txt");
+
+    // Check if files were opened successfully
+    if (file1 != nullptr && file2 != nullptr) {
+        // Write data to file1.txt
+        vector<char> dataToWrite = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
+        int writeResult = file1->write(dataToWrite);
+        cout << "Write to file1.txt result: " << writeResult << endl;
+
+        // Read from file1.txt
+        vector<char> file1Contents = file1->read();
+        cout << "Contents of file1.txt: ";
+        for (char c : file1Contents) {
+            cout << c;
+        }
+        cout << endl;
+
+        // Read from file2.txt (expecting empty contents)
+        vector<char> file2Contents = file2->read();
+        cout << "Contents of file2.txt: ";
+        for (char c : file2Contents) {
+            cout << c;
+        }
+        cout << endl;
+
+        // Close the files
+        fileSystem->closeFile(file1);
+        fileSystem->closeFile(file2);
     } else {
-        std::cerr << "Error creating file: " << result ;
-    }
-cout<<endl;
-
-    std::cout << "Test Case 2: Attempt to create a file with an invalid extension";
-    result = fileSystem->createFile("File.invalidExtension");
-    if (result == UEXTN) {
-        std::cout << "Correctly detected invalid file extension.";
-    } else {
-        std::cerr << "Error creating file: " << result ;
-    }
-
-    cout<<endl;
-
-    std::cout << "Test Case 3: Open an existing file";
-    AbstractFile* openedFile = fileSystem->openFile("test.txt");
-    if (openedFile != nullptr) {
-        std::cout << "File 'test.txt' opened successfully";
-    } else {
-        std::cerr << "Error opening file";
-    }
-
-    cout<<endl;
-    std::cout << "\nTest Case 4: Attempt to open a non-existing file";
-    AbstractFile* nonExistingFile = fileSystem->openFile("nonExistingFile.txt");
-    if (nonExistingFile == nullptr) {
-        std::cout << "Correctly detected non-existing file";
-    } else {
-        std::cerr << "Error: Opened a non-existing file";
-    }
-    cout<<endl;
-
-
-    result = fileSystem->closeFile(openedFile);
-    if (result == OK) {
-        std::cout << "File closed successfully";
-    } else {
-        std::cerr << "Error closing file: " << result;
-    }
-    cout<<endl;
-
-
-    result = fileSystem->closeFile(openedFile);
-    if (result == FCLSD) {
-        std::cout << "Correctly detected attempt to close a non-open file";
-    } else {
-        std::cerr << "Error: Closed a file that was not open";
+        std::cout << "Failed to open files for reading and writing." << std::endl;
     }
 
-    cout<<endl;
-
-
-
-    result = fileSystem->deleteFile("test.txt");
-    if (result == OK) {
-        std::cout << "File 'test.txt' deleted successfully";
-    } else {
-        std::cerr << "Error deleting file: " << result ;
-    }
-
-    cout<<endl;
-    std::cout << "Test Case 9: Attempt to delete a non-existing file";
-    result = fileSystem->deleteFile("nonExistingFile.txt");
-    if (result == UFILE) {
-        std::cout << "detected attempt to delete a non-existing file";
-    } else {
-        std::cerr << "Deleted a non-existing file";
-    }
-
-
-
+    // Clean up
+    delete fileSystem;
+    delete fileFactory;
 
     return 0;
 }
