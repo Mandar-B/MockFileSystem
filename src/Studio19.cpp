690de814dc581fb70c45291668a927164db18382
Nathaniel Hayman <nathanielhayman@gmail.com>
2023-12-15 18:26:49 -0600

    studio 19 tests

--- src/Studio19.cpp
+++ src/Studio19.cpp
@@ -1,26 +1,67 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include "mockos/ImageFile.h"
 #include "mockos/TextFile.h"
 #include "mockos/BasicDisplayVisitor.h"
 #include "mockos/MetadataDisplayVisitor.h"
-
 #include "mockos/SimpleFileSystem.h"
-#include <iostream>
+
+void print_test(string name, int& num, int res) {
+    cout << num++ << ": " << name << "\t" 
+        << (res ? "ERROR; code " : "SUCCESS") << (res ? to_string(res) : "") << endl;
+}
 
 int main() {
     // Create TextFile
+    TextFile ft("text.txt");
 
-        // Create TextFile and ImageFile
-        TextFile textFile("example.txt");
-        ImageFile imageFile("example.img");
+    const string inp_str = "some initial example input"; 
+    const vector<char> data(inp_str.begin(), inp_str.end());
 
-        // Create MetadataDisplayVisitor
-        MetadataDisplayVisitor metadataDisplayVisitor;
+    ft.write(vector<char>());
 
-        // Test by calling accept on the file objects
-        textFile.accept(&metadataDisplayVisitor);
-        imageFile.accept(&metadataDisplayVisitor);
+    vector<char> contents = ft.read();
 
-        return 0;
+    for (char c : contents) {
+        cout << c;
+    }
+    cout << endl;
 
+    ft.write(data);
+
+    contents = ft.read();
+
+    for (char c : contents) {
+        cout << c;
+    }
+    cout << endl;
+
+    contents[3] = 'X';
+    contents[7] = 'Z';
+
+    ft.write(contents);
+
+    contents = ft.read();
+
+    for (char c : contents) {
+        cout << c;
+    }
+    cout << endl;
+
+    // Create MetadataDisplayVisitor
+    MetadataDisplayVisitor metadataDisplayVisitor;
+
+    ft.accept(&metadataDisplayVisitor);
+
+    ImageFile img("image.img");
+
+    const string image_content = "X  X  X  X3";
+    const vector<char> data2(image_content.begin(), image_content.end());
+
+    img.write(data2);
+
+    img.accept(&metadataDisplayVisitor);
+
+    return 0;
 }
